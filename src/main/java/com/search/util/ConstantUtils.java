43ddc91ff8722594df49bac22e99807929d8e013
psai1162 <psai1162@gmail.com>
2019-08-13 02:04:38 -0500

    removed unused files

--- src/main/java/com/search/util/ConstantUtils.java
+++
@@ -1,6 +0,0 @@
-package com.search.util;
-
-public class ConstantUtils {
-     public static String INPUT_DIRECTORY_PATH ="/Users/praveenapardesi/Downloads/sample_text";
-     public static String INDEX_CREATED_PATH ="/Users/praveenapardesi/Downloads/sample_output";
-}
